0a0c58a73db1add547a2703c1231bf46a319f47d
CC-KEH <arbashhussain08@gmail.com>
2023-06-11 18:57:15 +0530

    basic speech recognition and output functionality done

---
+++ README.md
@@ -0,0 +1,17 @@
+# Requirements:
+
+## Python 3.11.2
+# Setup
+## After cloning run the following commands:
+    pip install speechrecognition   
+    pip install wikipedia
+    pip install openai
+    install pyAudio:
+        1. Pycharm - just install from packages
+        2. VSCODE or other editor - goto "https://www.lfd.uci.edu/~gohlke/pythonlibs/" click on pyaudio, download for your python version, put the downloaded whl file in project folder and run "pip install 'file name' " 
+    For WINDOWS -> python -m pip install --upgrade pywin32 
+    For MacOs -> no need
+
+
+
+    
--- ai-assistant/main.py
+++ ai-assistant/main.py
@@ -1,4 +1,26 @@
+import speech_recognition as sr
+import os
+import win32com.client
+speaker = win32com.client.Dispatch('SAPI.SpVoice')
 
+def say(text):
+    speaker.speak(f"{text}") 
 
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio,language='en-in')
+            print(f'user said {query}')
+            return query
+        except Exception as e:
+            return "Sorry, I could'nt catch what you were saying."
+            
+            
 if __name__ =="__main__":
-    print('HELOO')
+    while True:
+        print('Listening...')
+        text = take_command()
+        say(text)
