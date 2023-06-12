addfb9349c174da3f79ec41d120ffb1595a952e6
CC-KEH <arbashhussain08@gmail.com>
2023-06-12 16:09:39 +0530

    Spotify Integration

--- README.md
+++ README.md
@@ -6,11 +6,18 @@
     pip install speechrecognition   
     pip install wikipedia
     pip install openai
+    pip install spotipy
     install pyAudio:
         1. Pycharm - just install from packages
         2. VSCODE or other editor - goto "https://www.lfd.uci.edu/~gohlke/pythonlibs/" click on pyaudio, download for your python version, put the downloaded whl file in project folder and run "pip install 'file name' " 
     For WINDOWS -> python -m pip install --upgrade pywin32 
     For MacOs -> no need
+
+## Spotify Setup
+### Create a spotify [developer account](https://developer.spotify.com/)
+### Goto Dashboard, create App, fill details, ignore Website box, write http://localhost/4000 in RedirectURL.
+### After creating app Get client id, client secret and replace it with the clientID, clientSecret in main.py
+### You are good to go.
 
 
 
--- ai-assistant/main.py
+++ ai-assistant/main.py
@@ -1,7 +1,36 @@
 import speech_recognition as sr
 import os
 import win32com.client
+import webbrowser
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 speaker = win32com.client.Dispatch('SAPI.SpVoice')
+
+
+clientID = '<ClientID>'
+clientSecret = '<ClientSecretID>'
+redirect_uri = 'http://localhost/4000'
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientID,client_secret=clientSecret))
+
+while True:
+    print("Welcome, C.C")
+    print("0 - Exit")
+    print("1 - Search for a Song")
+    choice = int(input("Your Choice: "))
+    if choice == 1:
+        searchQuery = input("Enter Song Name: ")
+        searchResults = sp.search(searchQuery,1,0,"track")
+        tracks_dict = searchResults['tracks']
+        tracks_items = tracks_dict['items']
+        song = tracks_items[0]['external_urls']['spotify']
+        webbrowser.open(song)
+        print('Song has opened in your browser.')
+    elif choice == 0:
+        break
+    else:
+        print("Enter valid choice.")
 
 def say(text):
     speaker.speak(f"{text}") 
@@ -9,7 +38,6 @@ def say(text):
 def take_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1
         audio = r.listen(source)
         try:
             query = r.recognize_google(audio,language='en-in')
@@ -20,7 +48,15 @@ def take_command():
             
             
 if __name__ =="__main__":
+    sites = [['youtube','https://youtube.com'],['google','https://google.com'],['twitch','https://twitch.tv'],['wikipedia','https://wikipedia.com']]
     while True:
         print('Listening...')
-        text = take_command()
-        say(text)
+        query = take_command()
+        for site in sites:
+            if(f"Open {site[0]}".lower() in query.lower()):
+                say(f"opening {site[0]}")
+                webbrowser.open(f'{site[1]}')
+        if('open music' in query):
+            musicPath = '<Path>'
+            os.startfile(musicPath)
+            
