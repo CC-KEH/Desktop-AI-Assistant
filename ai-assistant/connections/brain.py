2219f0b250b991bcca8461746bfc562162ec52c9
CC-KEH <arbashhussain08@gmail.com>
2024-06-30 10:59:50 +0530

    langchain works

--- ai-assistant/connections/brain.py
+++ ai-assistant/connections/brain.py
@@ -1,36 +1,80 @@
 import os
-import sys
-from dotenv import load_dotenv
-from llama_index.core.agent import ReActAgent
-from llama_index.llms.gemini import Gemini
-from saved_data.prompts import *
-from tools.rag import tools
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(parent_dir)
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from PyPDF2 import PdfReader
+
+from saved_data.prompts import *
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getenv("gemini_api_key")
+pdfs_path = "saved_data/pdfs/"
+genai.configure(api_key = os.environ["gemini_api_key"])
+
+class VectorStorePipeline:
+    def get_pdfs(self,pdfs_path):
+        pdfs = []
+        for pdf in os.listdir(pdfs_path):
+            pdfs.append(pdfs_path + pdf)
+        return pdfs
+    
+    def get_pdf_text(self,pdf_docs):
+        text = ""
+        for pdf in pdf_docs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        return text
+
+    def get_text_chunks(self,text):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+        chunks = text_splitter.split_text(text)
+        return chunks
+
+
+    def get_vector_store(self,text_chunks):
+        embeddings = GoogleGenerativeAIEmbeddings(google_api_key=os.environ["gemini_api_key"],model='models/embedding-001')
+        vector_store = FAISS.from_texts(text_chunks, embeddings)
+        vector_store.save_local('faiss_index')
+
 
 class Brain:
     def __init__(self):
-        self.llm = Gemini(model="models/gemini-pro",)
-        self.agent = ReActAgent.from_tools(
-            tools, llm=self.llm, verbose=True, context=context_str)
-        
-    def rag(self, question):
-        response = self.agent.query(question)
-        return response.text
+        self.llm = ChatGoogleGenerativeAI(google_api_key=os.environ["gemini_api_key"],model='gemini-pro',temperature=0.3)
     
+    def get_conversational_chain(self):
+        self.prompt = PromptTemplate(template=rag_prompt,input_variables=['context','question'])
+        self.chain = load_qa_chain(self.llm,chain_type='stuff',prompt=self.prompt)
+        return self.chain
+
     def ask(self,question):
-        response = self.llm.complete(question)
-        return response
+        response = self.llm.invoke(question)
+        return dict(response)['content']
+    
+    def process_user_input(self,question):
+        embeddings = GoogleGenerativeAIEmbeddings(google_api_key=os.environ["gemini_api_key"],model='models/embedding-001')
+        new_db = FAISS.load_local('faiss_index',embeddings,allow_dangerous_deserialization=True)
+        docs = new_db.similarity_search(question)
+        chain = self.get_conversational_chain()
+        response = chain({"input_documents": docs,"question": question},return_only_outputs=True)
+        return response['output_text']
     
 if __name__ == '__main__':
     brain = Brain()
     context = context_str
     question = "What is your name?"
-    print(brain.ask(f"{context},+question: {question}"))
-    rag_question = "What is a RAG?"
-    brain.rag(f"question: {rag_question}")
+    response = brain.ask(f"{context},+question: {question}")
+    print(response)
+    # Rag
+    vs = VectorStorePipeline()
+    pdfs = vs.get_pdfs(pdfs_path)
+    text = vs.get_pdf_text(pdfs)
+    chunks = vs.get_text_chunks(text)
+    vs.get_vector_store(chunks)
+    
+    print(brain.process_user_input("What is Advanced RAG?"))
